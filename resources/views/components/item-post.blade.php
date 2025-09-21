722281bec362f8ffae1e3ac5d600116ab8270bba
DTM-ThangHV <vanthoa225@gmail.com>
2025-09-21 10:17:40 +0700

    Fix

--- resources/views/components/item-post.blade.php
+++ resources/views/components/item-post.blade.php
@@ -11,7 +11,7 @@ $route = route('page',['cate_slug' => $post->category?->slug, 'slug' => $post['s
 
         <div class="content-section" title="{{$post['title']}}">
             <a href="{{$route}}">
-                <h2 class="title text-223f81">{{$post['title']}} {{$post['title']}}</h2>
+                <h2 class="title text-223f81">{{$post['title']}}</h2>
             </a>
             <div class="box-text" style="padding: 3px 0 10px;">
                 <div class="news-date" style="margin-bottom: 0">
